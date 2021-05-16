6fcfec3bb41369986f390155f742568fb18dab2a
yuya-kajiwara0315 <tamanigame@gmail.com>
2021-05-16 10:15:05 +0900

    空のコメント投稿時にエラーを修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,7 +5,8 @@ class CommentsController < ApplicationController
       redirect_to prototype_path(@comment.prototype)
     else
      @prototype = @comment.prototype
-      render "prototypes/show"
+     @comments = @prototype.comments
+     render "prototypes/show"
     end
   end
 
